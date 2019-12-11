c6607ef13f26216642f7b0eef01736d9b6052bee
Alejandro Yunes <alejandroyunes@outlook.com>
2019-12-10 22:39:40 -0500

    menu and drawer implemented

--- App.js
+++ App.js
@@ -1,89 +1,103 @@
+
 import React from "react";
-import { Button, View, Text } from "react-native";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { Image, StyleSheet, View, Text } from "react-native";
+
+import { ThemeProvider, Header, Button, Icon } from "react-native-elements";
+
+import { createAppContainer, } from "react-navigation";
+
+import { createDrawerNavigator } from "react-navigation-drawer";
 
 
 
-class HomeScreen extends React.Component {
+// Later on in your component
+
+
+class MyHomeScreen extends React.Component {
   static navigationOptions = {
-    title: "Home page"
+    drawerLabel: "Home",
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require("./assets/NavLogo.png")}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    )
   };
+
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => {
-            /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate("Details", {
-              itemId: 86,
-              otherParam: "anything you want here"
-            });
-          }}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  static navigationOptions = {
-    title: "Details"
-  };
-
-  render() {
-    /* 2. Get the param, provide a fallback value if not available */
-    const { navigation } = this.props;
-    const itemId = navigation.getParam("itemId", "NO-ID");
-    const otherParam = navigation.getParam("otherParam", "some default value");
-
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Details Screen</Text>
-        <Text>itemId: {JSON.stringify(itemId)}</Text>
-        <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() =>
-            this.props.navigation.push("Details", {
-              itemId: Math.floor(Math.random() * 100)
-            })
+      <ThemeProvider>
+        <Header
+          leftComponent={
+            <Icon
+              name="menu"
+              color="#fff"
+              onPress={() => this.props.navigation.openDrawer()}
+            />
           }
+          centerComponent={{ text: "Aprende React", style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
         />
-        <View>
-          <FontAwesomeIcon icon={faCoffee} style={style.icon} />
-        </View>
-
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
+      </ThemeProvider>
     );
   }
 }
 
-const RootStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Details: DetailsScreen
-  },
-  {
-    initialRouteName: "Home"
-  }
-);
 
-const AppContainer = createAppContainer(RootStack);
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: "Notifications",
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require("./assets/NavLogo.png")}
+        style={[styles.icon, { tintColor: tintColor }]}
+      />
+    )
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
+}
+     
+
+
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24
+  },
+  buttonPadding: {
+    paddingTop: 10,
+  }
+});
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen
+  },
+  Notifications: {
+    screen: MyNotificationsScreen
+  }
+});
+
+
+
+
+
+const MyApp = createAppContainer( MyDrawerNavigator);
 
 export default class App extends React.Component {
   render() {
-    return <AppContainer />;
+    return <MyApp />;
   }
 }
+
+
